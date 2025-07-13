2b27a860d504998e815a4410dddc5d4a785eb358
Oleksiy <Unialex@ukr.net>
2025-07-13 14:06:16 +0300

    validation done

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pinoLogger from 'pino-http';
 import corse from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import router from './routers/contacts.js';
+import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
@@ -18,7 +18,10 @@ export const setupServer = () => {
   httpServer.use(express.json()); // for JSON-body
   httpServer.use(express.urlencoded({ extended: true })); // for formData-body
 
-  httpServer.use(router);
+  httpServer.get('/', (request, response) => {
+    response.send('Welcome to "Contacts book". Please pass to /contacts');
+  });
+  httpServer.use('/contacts', contactsRouter);
 
   httpServer.use(notFoundHandler);
   httpServer.use(errorHandler);
